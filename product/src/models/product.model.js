3366366352f18acb520a4f6cea859924d726b4a9
Manish Kumar <manishkr.smi@gmail.com>
2026-03-31 18:18:43 +0530

    feat(product): add stock field to product schema with default value

--- product/src/models/product.model.js
+++ product/src/models/product.model.js
@@ -29,7 +29,11 @@ const productSchema = new mongoose.Schema({
         url: String,
         thumbnail: String,
         id: String
-    }]
+    }],
+    stock: {
+        type: Number,
+        default: 0
+    }
 })
 
 // indexing for text search on title and description
