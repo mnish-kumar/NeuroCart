e34b728443072588ca8d2c11d62499235d27b62d
Manish Kumar <manishkr.smi@gmail.com>
2026-04-12 15:59:49 +0530

    feat(server): add RabbitMQ connection to server initialization

--- auth/server.js
+++ auth/server.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./src/db/db');
+const { connectRabbitMQ } = require('./src/brokers/broker');
 
-
+// Connnect to Database
 connectDB();
+
+// Connect to RabbitMQ
+connectRabbitMQ();
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
