9c266263cd7fe132fd6ae81ac8c4c2f7e3e4aa09
Manish Kumar <manishkr.smi@gmail.com>
2026-04-12 16:04:32 +0530

    feat(broker): implement RabbitMQ connection and message handling

--- payment/server.js
+++ payment/server.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./src/db/db');
+const { connectRabbitMQ } = require('./src/broker/broker');
 // const PORT = process.env.PORT;
 
 // Connect to the database and start the server
 connectDB();
+
+// Connect to RabbitMQ
+connectRabbitMQ();
 
 
 app.listen(3004, () => {
